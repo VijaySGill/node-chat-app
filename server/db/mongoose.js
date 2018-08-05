ee608adf1755ebc3171a4843774ad0b2a6d174e8
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-08-05 17:54:09 +0100

    Improved functionality

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ChatApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGOLAB_ORANGE_URI || 'mongodb://localhost:27017/ChatApp', { useNewUrlParser: true });
+console.log(process.env.MONGOLAB_ORANGE_URI);
 
 module.exports = {
   mongoose: mongoose

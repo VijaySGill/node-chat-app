efbc1185d5b741171afadc6df01a64e95c3070a9
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-08-05 22:11:22 +0100

    Further fleshing out

--- server/server-login.js
+++ server/server-login.js
@@ -12,8 +12,42 @@ app.use(bodyParser.json());
 
 app.post('/users', function(request, response)
 {
-  var user = response.body;
-  console.log(user);
+  var body = _.pick(request.body, ['name', 'email', 'username', 'password']);
+
+  var user = new User(body);
+
+  user.save().then(function(document)
+  {
+    response.send(document);
+  }).catch(function(error)
+  {
+    response.status(400).send(error);
+  })
+});
+
+app.get('/users', function(request, response)
+{
+  User.find().toArray().then(function(documents)
+  {
+    console.log('Users in the database:');
+    console.log(JSON.stringify(documents, undefined, 2));
+  }, function(error)
+  {
+    console.log('Unable to fetch users', error);
+  });
+
+  // User.find({
+  //   _creator: request.user._id // find all todos created by currently logged-in user
+  // }).then(function(todos)
+  // {
+  //   response.send(
+  //   {
+  //     todos: todos // easier to send as an object if ever I need to tack something on
+  //   });
+  // }, function(error)
+  // {
+  //   response.status(400).send(error);
+  // });
 });
 
 app.listen(port, function()

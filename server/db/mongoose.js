c14cc503a7672cfaa89c2fb329d50ef3b429684d
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-08-05 18:06:02 +0100

    Roll back changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_ORANGE_URI || 'mongodb://localhost:27017/ChatApp', { useNewUrlParser: true });
-console.log(process.env.MONGOLAB_ORANGE_URI);
+mongoose.connect('mongodb://localhost:27017/ChatApp', { useNewUrlParser: true });
 
 module.exports = {
   mongoose: mongoose

3b600a4103378a9512632985873b5521fe37ddda
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-08-06 18:54:15 +0100

    Further refactoring to rollback previous changes

--- server/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ChatApp', { useNewUrlParser: true });
-
-module.exports = {
-  mongoose: mongoose
-};

d0e33481e89db5bb8d5b89d7247a8dfc0715a502
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-08-23 18:43:56 +0100

    Mongoose connection refactoring

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,8 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ChatApp', { useNewUrlParser: true });
 
 module.exports = {
-    mongoose
-}
+  mongoose: mongoose
+};

bd4bad081f1552a5fa626f680187e7a9ce8c8887
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-08-24 14:22:26 +0100

    TEST: Added production environment

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,17 @@ if(env === 'development' || env === 'test')
   });
 }
 
+else if(env === 'production')
+{
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach(function(key)
+  {
+    process.env[key] = envConfig[key]
+  });
+}
+
 if (process.env.NODE_ENV === 'production' && !process.env.JWT_SECRET)
 {
   throw new Error('JWT_SECRET is required in production!');
